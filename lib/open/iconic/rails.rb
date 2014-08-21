c9c1600390b765f0a674d518b1e70cf4cea657cf
Michael Lee <michael@mrlee.io>
2014-08-20 22:34:47 -0700

    Initial commit

---
+++ lib/open/iconic/rails.rb
@@ -0,0 +1,9 @@
+require "open/iconic/rails/version"
+
+module Open
+  module Iconic
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/open/iconic/rails/version.rb
@@ -0,0 +1,7 @@
+module Open
+  module Iconic
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
